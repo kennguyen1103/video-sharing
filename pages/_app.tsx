ad88ad2723ade6dd52529cd776c8c807e38c540a
Ken Nguyen <nln.vn.it@gmail.com>
2021-11-14 10:10:37 +0700

    Add recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { RecoilRoot } from "recoil";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </ChakraProvider>
   );
 }
